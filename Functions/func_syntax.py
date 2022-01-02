ebfdb51aed7df63487a91f30f41d994a2355e93f
dwijendra626 <dwijendra626@gmail.com>
2022-01-02 22:32:54 +0530

    function syntax

---
+++ Functions/func_syntax.py
@@ -0,0 +1,20 @@
+def fun():  
+    '''
+    Its function definition 
+    Here, function is defined i.e., whatever operations or calculations we have to perform 
+    write those operations here. 
+    '''
+    
+    pass
+
+fun()    # function call ---> here, values are stored which is required for function definition 
+
+'''
+e.g. 
+---
+def fun(a,b):
+    print(a,b)
+fun(1,2)
+
+# o/p --> 1 2
+'''
